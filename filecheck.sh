674f1409b743b6fff98087aacd578049f5c12984
mayurgavhane <mayur.gavhane29@gmail.com>
2025-02-16 13:37:09 +0100

    cleanup

--- filecheck.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while [ -f ~/test.txt ]; do 
-    echo "$(date) The test file exist"
-sleep 1
-done
-    
-    echo "$(date) The file does not exist" 
-
--- forloop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for NAMES in $(cat names.txt); do
-
-    grep "M*"| echo "Names are: $NAMES"
-done
