0c2e3d8ea56ea732f1f22c9865798bf33930852f
Mayur Gavhane <78908010+mayurgavhane@users.noreply.github.com>
2025-02-22 17:50:26 +0100

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,16 @@
+temp = 50
+forecast = "sunny"
+if temp > 40 and forecast != "rainy":
+     print ("Enjoy Outdoor")
+    
+else:
+    print ("Stay Home and enjoy")
+
+forecast = "sunny"
+
+if not forecast == "rainy":
+     print ("Go out and enjoy")
+
+else:
+     print ("stay inside")
+
