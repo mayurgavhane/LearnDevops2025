70f9729be00b89230ff0d4d5aeb453f4904986b7
mayurgavhane <mayur.gavhane29@gmail.com>
2025-02-22 19:35:27 +0100

    added small text

--- practice.py
+++ practice.py
@@ -1,7 +1,7 @@
 temp = 50
 forecast = "sunny"
 if temp > 40 and forecast != "rainy":
-     print ("Go and Enjoy Outdoor")
+     print ("Go and Enjoy Outdoor and drink some wine")
     
 else:
     print ("Stay Home and enjoy")
